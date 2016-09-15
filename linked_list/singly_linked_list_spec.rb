de3095814502086132441206702f19d26bdcd5b1
David Maurer <davemaurermail@gmail.com>
2016-09-14 22:32:51 -0600

    Add test for telling user when all_nodes are asked for on an
    empty list. Also remove the l from the expectation syntax in rspec tests.

--- linked_list/singly_linked_list_spec.rb
+++ linked_list/singly_linked_list_spec.rb
@@ -4,81 +4,81 @@ describe SinglyLinkedList do
   let (:list) { SinglyLinkedList.new('MyList') }
 
   it 'has a head' do
-    expect(list.head.data).to eql('head')
+    expect(list.head.data).to eq('head')
   end
 
   it 'can add nodes' do
     list.push_node('Sith')
 
-    expect(list.head.next_node.data).to eql('Sith')
+    expect(list.head.next_node.data).to eq('Sith')
   end
 
   it 'can find its tail' do
     list.push_node('Sith')
 
-    expect(list.find_tail.data).to eql('Sith')
+    expect(list.find_tail.data).to eq('Sith')
   end
 
   it 'can add multiple nodes' do
     list.push_node('Jedi')
     list.push_node('Sith')
 
-    expect(list.head.next_node.data).to eql('Jedi')
-    expect(list.find_tail.data).to eql('Sith')
+    expect(list.head.next_node.data).to eq('Jedi')
+    expect(list.find_tail.data).to eq('Sith')
   end
 
   it 'can find nodes by data' do
     list.push_node('hidden rebel base')
 
-    expect(list.find_by_data('hidden rebel base').data).to eql('hidden rebel base')
+    expect(list.find_by_data('hidden rebel base').data).to eq('hidden rebel base')
   end
 
   it 'can pop off the last node' do
     list.push_node('Chewy')
     list.push_node('Han')
 
-    expect(list.find_tail.data).to eql('Han')
+    expect(list.find_tail.data).to eq('Han')
 
     list.pop
 
-    expect(list.find_tail.data).to eql('Chewy')
+    expect(list.find_tail.data).to eq('Chewy')
 
     list.pop
 
-    expect(list.find_tail.data).to eql('head')
+    expect(list.find_tail.data).to eq('head')
   end
 
   it 'tells you when you try to pop the last node off an empty list' do
-    expect(list.pop).to eql('This list has no nodes')
+    expect(list.pop).to eq('This list has no nodes')
   end
 
   it 'can prepend a node' do
     list.push_node('Lando')
 
-    expect(list.find_tail.data).to eql('Lando')
+    expect(list.find_tail.data).to eq('Lando')
 
     list.prepend_node('R2')
 
-    expect(list.head.next_node.data).to eql('R2')
-    expect(list.find_tail.data).to eql('Lando')
+    expect(list.head.next_node.data).to eq('R2')
+    expect(list.find_tail.data).to eq('Lando')
   end
 
   it 'can delete a node by data' do
     list.push_node('Hoth')
     list.push_node('Tantooine')
 
-    expect(list.head.next_node.data).to eql('Hoth')
-    expect(list.delete_node('Hoth').data).to eql('Hoth')
-    expect(list.head.next_node.data).to eql('Tantooine')
+    expect(list.head.next_node.data).to eq('Hoth')
+    expect(list.delete_node('Hoth').data).to eq('Hoth')
+    expect(list.head.next_node.data).to eq('Tantooine')
   end
 
   it 'tells you if you try to delete a node that isnt in the list' do
-    expect(list.delete_node('Hoth')).to eql('No such node exists')
+    expect(list.delete_node('Hoth')).to eq('No such node exists')
 
     list.push_node('Yoda')
     list.push_node('Obi-Wan')
 
-    expect(list.delete_node('Darth Maul')).to eql('No such node exists')
+    expect(list.delete_node('Darth Maul')).to eq('No such node exists')
   end
 
   it 'can delete entire list' do
@@ -88,25 +88,25 @@ describe SinglyLinkedList do
     list.push_node('Yoda')
     list.push_node('Obi-Wan')
 
-    expect(list.count).to eql(5)
+    expect(list.count).to eq(5)
 
     list.delete_all
 
-    expect(list.count).to eql(0)
+    expect(list.count).to eq(0)
   end
 
   it 'can count its nodes excluding head' do
-    expect(list.count).to eql(0)
+    expect(list.count).to eq(0)
 
     list.push_node('Chewy')
 
-    expect(list.count).to eql(1)
+    expect(list.count).to eq(1)
 
     list.push_node('Han')
     list.push_node('Hoth')
     list.push_node('Tantooine')
 
-    expect(list.count).to eql(4)
+    expect(list.count).to eq(4)
   end
 
   it 'can return all nodes' do
@@ -114,6 +114,10 @@ describe SinglyLinkedList do
     list.push_node('Han')
     list.push_node('Hoth')
 
-    expect(list.all_nodes).to eql(['Chewy', 'Han', 'Hoth'])
+    expect(list.all_nodes).to eq(['Chewy', 'Han', 'Hoth'])
+  end
+
+  it 'tells you when you ask for all nodes in an empty list' do
+    expect(list.all_nodes).to eq('This list has no nodes')
   end
 end
