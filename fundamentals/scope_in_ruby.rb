dd217643286fdd404aea30645e12116a919b8bf0
David Maurer <davemaurermail@gmail.com>
2016-09-27 17:29:26 -0600

    introduce variable scope and block scope in scope.rb

---
+++ fundamentals/scope_in_ruby.rb
@@ -0,0 +1,10 @@
+# Reference:
+# https://www.sitepoint.com/understanding-scope-in-ruby/
+# http://www.techotopia.com/index.php/Ruby_Variable_Scope
+
+# The term 'scope' is a concept, and the way scope is implemented in many languages is hard to explain with just one
+# overarching statement. Ruby scope is often called 'variable scope'. Ruby also uses 'block scope'. Don't try to think
+# of them as different concepts. Just learn how to use scope different variables, and by creating variables within blocks.
+# Then you can dig into what Ruby scope really is, and maybe write a blog post about it!
+
+
