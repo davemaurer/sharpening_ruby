22818121608885be1f803cdaa64442a266171584
David Maurer <davemaurermail@gmail.com>
2016-09-28 22:30:11 -0600

    All tests passing for unicorn.

--- mythical-creatures/unicorn/unicorn.rb
+++ mythical-creatures/unicorn/unicorn.rb
@@ -1,8 +1,16 @@
 class Unicorn
-  attr_reader :name
+  attr_reader :name, :color
 
   def initialize(name, color='white')
     @name = name
     @color = color
+  end
+
+  def white?
+    @color == 'white'
+  end
+
+  def say(phrase)
+    '**;* ' + phrase + ' **;*'
   end
 end
