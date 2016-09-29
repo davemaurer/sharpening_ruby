aa816b52678a26cd579d982ea836b11dd74662d1
David Maurer <davemaurermail@gmail.com>
2016-09-28 22:58:20 -0600

    Last test passing using a hash and checking/changing the key value pairs.

--- mythical-creatures/dragon/dragon.rb
+++ mythical-creatures/dragon/dragon.rb
@@ -5,10 +5,18 @@ class Dragon
     @name  = name
     @rider = rider
     @color = color
-    @hungry = true
+    @hungry = {hungry: true, times_eaten: 0}
   end
 
   def hungry?
-    @hungry
+    @hungry[0] = true unless full?
+  end
+
+  def eat
+    @hungry[:times_eaten] += 1
+  end
+
+  def full?
+    @hungry[:times_eaten] >= 3
   end
 end
