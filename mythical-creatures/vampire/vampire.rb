1887abda97c306e2cef99ae46413d1f53f2676fb
David Maurer <davemaurermail@gmail.com>
2016-09-28 22:38:44 -0600

    All vampire test passing.

--- mythical-creatures/vampire/vampire.rb
+++ mythical-creatures/vampire/vampire.rb
@@ -4,5 +4,14 @@ class Vampire
   def initialize(name, pet='bat')
     @name = name
     @pet  = pet
+    @thirsty = true
+  end
+
+  def thirsty?
+    @thirsty
+  end
+
+  def drink
+    @thirsty = false
   end
 end
