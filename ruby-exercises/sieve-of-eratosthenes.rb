b7b69183966ed8bfcc3c79578b80678fc969676b
David Maurer <davemaurermail@gmail.com>
2016-10-01 14:58:20 -0600

    Initial answer to sieve-of-eratosthenes.

--- ruby-exercises/sieve-of-eratosthenes.rb
+++ ruby-exercises/sieve-of-eratosthenes.rb
@@ -1,9 +1,23 @@
 class SieveIt
-  attr_reader :n
+  attr_reader :n, :numbers
 
-  def initialize(number)
-    @n = number
+  def initialize(num)
+    @n     = num
+    @numbers = (2..num).to_a
   end
 
-  
+  def eliminate_composite_numbers
+    composites = []
+    numbers.each do |num|
+      next_num = num + num
+      until next_num > n
+        composites << next_num
+        next_num = next_num + num
+      end
+    end
+    return numbers - composites
+  end
 end
+
+sieve = SieveIt.new(20)
+puts sieve.eliminate_composite_numbers
