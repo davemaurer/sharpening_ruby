a659534f32ea9a4f4b1f9bf63a546f8efe1e645d
David Maurer <davemaurermail@gmail.com>
2016-10-01 17:04:04 -0600

    Ogre hits human every second swing test passing. Needed encounter_counter on ogre this time.
    Already have one on human so not needed? Or maybe showing you can have two instance variables named
    the same thing holding different values thanks to instance scope. Add @knocked_out instance variable
    and knocked_out? method to human. Swing at method in ogre changes knocked_out to true if conditional met.

--- mythical-creatures/ogre/human.rb
+++ mythical-creatures/ogre/human.rb
@@ -1,13 +1,18 @@
 class Human
   attr_reader :name
-  attr_accessor :encounter_counter
+  attr_accessor :encounter_counter, :knocked_out
 
   def initialize(name= 'Jane')
-    @name = name
+    @name              = name
     @encounter_counter = 0
+    @knocked_out       = false
   end
 
   def notices_ogre?
     encounter_counter % 3 == 0
+  end
+
+  def knocked_out?
+    @knocked_out
   end
 end
--- mythical-creatures/ogre/ogre.rb
+++ mythical-creatures/ogre/ogre.rb
@@ -2,20 +2,23 @@ require_relative 'human'
 
 class Ogre
   attr_reader :name, :home
-  attr_accessor :times_swung
+  attr_accessor :times_swung, :encounter_counter
 
   def initialize(name, home= 'Swamp')
     @name        = name
     @home        = home
     @times_swung = 0
+    @encounter_counter = 0
   end
 
   def encounter(target)
+    @encounter_counter += 1
     target.encounter_counter += 1
     swing_at(target) if target.notices_ogre?
   end
 
   def swing_at(target)
     @times_swung += 1
+    target.knocked_out = true if @times_swung % 2 == 0
   end
 end
