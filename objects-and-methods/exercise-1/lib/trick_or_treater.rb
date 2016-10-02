66d673439e2a855929d1e3a13b33b6e754fda114
David Maurer <davemaurermail@gmail.com>
2016-10-01 22:40:03 -0600

    objects-and-methods trick or treat test has empty bag be default,
    has no candy in bag, and bag gets candies all passing.

--- objects-and-methods/exercise-1/lib/trick_or_treater.rb
+++ objects-and-methods/exercise-1/lib/trick_or_treater.rb
@@ -1,7 +1,12 @@
 class TrickOrTreater
-  attr_accessor :dressed_up_as
+  attr_accessor :dressed_up_as, :bag
 
   def initialize(costume)
     @dressed_up_as = costume.style
+    @bag = []
+  end
+
+  def has_candy?
+    !@bag.empty?
   end
 end
