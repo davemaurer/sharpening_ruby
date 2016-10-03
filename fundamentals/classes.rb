8175b70baf7641e86fdde3fe8637be0f6ac9b6d4
David Maurer <davemaurermail@gmail.com>
2016-10-02 23:29:31 -0600

    Initial description of classes in /fundamentals.

---
+++ fundamentals/classes.rb
@@ -0,0 +1,9 @@
+=begin
+Classes in Ruby are objects similar to methods, but with more control of scope and variables. If
+you want to hold variables, and methods that use those variables, you can use a class. Think of
+classes as the wrapper around methods and variables you want to carry around inside of your program.
+They are really just another way to organize your code so you can separate things neatly.
+
+The life of instance variables in a class last as long as the class is instantiated.
+
+=end
