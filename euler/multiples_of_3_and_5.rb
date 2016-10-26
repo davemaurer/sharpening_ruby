9ae06706fc00eefd4c481fb0f1bbbda01e51c9f2
David Maurer <davemaurermail@gmail.com>
2016-10-25 23:51:21 -0600

    second try for euler 1. chain methods on one line and reduce without needing to push
    onto new array..

--- euler/multiples_of_3_and_5.rb
+++ euler/multiples_of_3_and_5.rb
@@ -1,8 +1,10 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-multiples = []
+# multiples = []
+#
+# (1..1000).each { |number| multiples << number if number % 3 == 0 || number % 5 == 0 }
+#
+# puts multiples.reduce(:+)
 
-(1..1000).each { |number| multiples << number if number % 3 == 0 || number % 5 == 0 }
-
-puts multiples.reduce(:+)
+puts (1..1000).to_a.reduce(0) { |sum, num| (num % 3 == 0 || num % 5 == 0) ? sum + num : sum }
