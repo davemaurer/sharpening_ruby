3470fd319b7f66cea9796caf50da744876fa4933
David Maurer <davemaurermail@gmail.com>
2016-10-25 23:40:48 -0600

    First attempt at euler 1

---
+++ euler/multiples_of_3_and_5.rb
@@ -0,0 +1,8 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+multiples = []
+
+(1..1000).each { |number| multiples << number if number % 3 == 0 || number % 5 == 0 }
+
+puts multiples.reduce(:+)
