93b47712920ac26efc143c618767b817274fdfe6
David Maurer <davemaurermail@gmail.com>
2016-10-26 22:17:00 -0600

    Benchmark both ways. using .each is faster.

--- euler/multiples_of_3_and_5.rb
+++ euler/multiples_of_3_and_5.rb
@@ -1,10 +1,21 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
+require 'benchmark'
 
-# multiples = []
-#
-# (1..1000).each { |number| multiples << number if number % 3 == 0 || number % 5 == 0 }
-#
-# puts multiples.reduce(:+)
+iterations = 100000
 
-puts (1..1000).to_a.reduce(0) { |sum, num| (num % 3 == 0 || num % 5 == 0) ? sum + num : sum }
+Benchmark.bm do |bm|
+  bm.report do
+    iterations.times do
+      (1..1000).to_a.reduce(0) { |sum, num| (num % 3 == 0 || num % 5 == 0) ? sum + num : sum }
+    end
+  end
+  bm.report do
+    iterations.times do
+      multiples = []
+      (1..1000).each { |number| multiples << number if number % 3 == 0 || number % 5 == 0 }
+      multiples.reduce(:+)
+    end
+  end
+end
+
