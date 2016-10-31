d0aa457aca97d82eade7a749d64c1592ff9b84b2
David Maurer <davemaurermail@gmail.com>
2016-10-30 20:41:49 -0600

    Project euler 3 got something that works now.

--- euler/largest_prime_factor_e3.rb
+++ euler/largest_prime_factor_e3.rb
@@ -1,18 +1,22 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
-max = 600851475143
-numbers = (2..max/2)
-composites = []
 
-numbers.each do |num|
-  current = num * 2
-  until current >= max
-    composites << current
-    current += num
+def find_largest_prime_factor
+  number = 600851475143
+  largest = Math.sqrt(number).to_i
+  current = 2
+  result = 0
+  while current < largest
+    if number % current == 0
+      result = current
+      number = number / result
+    else
+      current += 1
+    end
   end
+  puts result
 end
 
-primes = numbers - composites.uniq
 
-puts primes.last
+find_largest_prime_factor
